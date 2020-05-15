3fbc8958443d9be401142b6fd10c4eb208f45c3a
ha doan <ha@vooy.co>
2020-05-15 22:54:34 +0800

    add unit test

--- NameSorter.Tests/NameConverterTest.cs
+++ NameSorter.Tests/NameConverterTest.cs
@@ -1,16 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NameSorter.Services;
 using Xunit;
+using Shouldly;
 
 namespace NameSorter.Tests
 {
     public class NameConverterTest
     {
         [Fact]
-        public void Name_Not_Valid_Converter()
+        public void Name_Blank_Not_Valid_Converter()
         {
-            
+            var nameValidation = new NameValidation();
+            nameValidation.IsValidName("").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Name_SingleName_Not_Valid_Converter()
+        {
+            var nameValidation = new NameValidation();
+            nameValidation.IsValidName("Ha").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Name_4GivenNames_Not_Valid_Converter()
+        {
+            var nameValidation = new NameValidation();
+            nameValidation.IsValidName("This is not valid name").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Name_3GivenNames_Valid_Converter()
+        {
+            var nameValidation = new NameValidation();
+            nameValidation.IsValidName("This is valid name").ShouldBeTrue();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Run the project
-`dotnet  dotnet run unsorted-names-list.txt sorted-names-list.txt`
+## Run the project
+Go to NameSorter folder, then run following command:
+
+`dotnet run unsorted-names-list.txt sorted-names-list.txt`
+
+
+
+## Run unit tests
+Go to NameSorter.Tests folder, then run following command:
+
+`dotnet test`
